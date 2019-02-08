5f5dd6ac886f64968df60f045dcdd03dbdfeb90d
djf604 <djjNv6Bzig>
2019-02-07 21:17:59 -0600

    Adding initial Django ORM skeleton

---
+++ models/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Team(models.Model):
+    team_id = models.IntegerField(primary_key=True)
+    team_location = models.CharField(max_length=32, null=True)
+    team_nickname = models.CharField(max_length=32, null=True)
+    team_abbrv = models.CharField(max_length=8, null=True)
+
+
+class TeamOwner(models.Model):
+    team = models.OneToOneField('Team', on_delete=models.CASCADE)
+    fname = models.CharField(max_length=32, null=True)
+    lname = models.CharField(max_length=32, null=True)
+
+
+class Matchup(models.Model):
+    week = models.IntegerField()
+    winner = models.BooleanField(help_text='1 for home team, 0 for away team')
+    home_team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    home_score = models.FloatField()
+    away_team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    away_score = models.FloatField()
+
+    def winning_team(self):
+        if self.winner:
+            return self.home_team
+        return self.away_team
